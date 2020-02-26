c50de7c83385fcad217ccc80b104e13569d5a65f
Dimitri <dimitri.bret@gmail.com>
2020-02-26 14:38:13 +0100

    feat: conditional modulo 2 pow 64

---
+++ bench/modulo_pe_tb.vhd
@@ -0,0 +1,37 @@
+-- @Author Dimitri Bret
+-- @Author Clement Dargein
+-- @description compteur test bench function for the AES algorithm 
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library AMNSLibrary;
+use AMNSLibrary.amns_definition_package.all;
+library source;
+use source.all;
+
+entity modulo_pe_tb is
+end entity  modulo_pe_tb;
+
+architecture modulo_pe_tb_arch of modulo_pe_tb is
+
+component modulo_pe is 
+			port (bit132_i: in bit132;
+                      en_i: in std_logic;
+                  bit132_o: out bit132);
+end component;
+
+signal bit132_i_s: bit132 :="101001010110010101100101011001010110010101100101011001010110010101100101011001010110010101100101011001010110010101100101011001010110";
+signal en_i_s: std_logic := '1';
+signal bit132_o_s: bit132;
+
+begin
+en_i_s <= '0' after 50 ns;
+MODULO_PE_MAP: modulo_pe port map ( bit132_i => bit132_i_s,
+                                  en_i => en_i_s,
+							  bit132_o => bit132_o_s);
+end modulo_pe_tb_arch;
+
+
+
+	
---
+++ sources/modulo_pe.vhd
@@ -0,0 +1,26 @@
+-- @author Dimitri Bret
+-- @author Clement Dargein
+-- @description Modulo 2^64 function on en_i signal in the processing unit.
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+library AMNSLibrary;
+use AMNSLibrary.amns_definition_package.all;
+
+entity modulo_pe is
+	port (
+        bit132_i: in bit132;
+		    en_i: in std_logic;
+        bit132_o: out bit132
+	   );
+end entity modulo_pe;
+
+
+architecture modulo_pe_arch of modulo_pe is
+
+    signal modulo132_s: bit132;
+    
+    begin
+        modulo132_s <= bit132_i and "000000000000000000000000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111111111111111111111";
+        bit132_o <= bit132_i when en_i = '0' else  modulo132_s;
+end architecture modulo_pe_arch;
